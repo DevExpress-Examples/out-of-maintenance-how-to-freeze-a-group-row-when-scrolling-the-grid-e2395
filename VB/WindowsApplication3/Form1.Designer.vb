898369757cd9feaf8f45c657bf8b349a1337ff75
DevExpressExampleBot <fakeEmail>
2019-02-16 02:51:28 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication3/Form1.Designer.vb
+++ VB/WindowsApplication3/Form1.Designer.vb
@@ -1,86 +1,83 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace WindowsApplication3
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace WindowsApplication3
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.repositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.EmbeddedNavigator.Margin = New System.Windows.Forms.Padding(2)
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.LookAndFeel.SkinName = "Blue"
-			Me.gridControl1.LookAndFeel.UseDefaultLookAndFeel = False
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Margin = New System.Windows.Forms.Padding(2)
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemPictureEdit1})
-			Me.gridControl1.Size = New System.Drawing.Size(483, 468)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.DetailHeight = 284
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.OptionsView.ColumnAutoWidth = False
-'			Me.gridView1.CustomDrawGroupRow += New DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventHandler(Me.gridView1_CustomDrawGroupRow);
-			' 
-			' repositoryItemPictureEdit1
-			' 
-			Me.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(483, 468)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Margin = New System.Windows.Forms.Padding(2)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.repositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.EmbeddedNavigator.Margin = New System.Windows.Forms.Padding(2)
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.LookAndFeel.SkinName = "Blue"
+            Me.gridControl1.LookAndFeel.UseDefaultLookAndFeel = False
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Margin = New System.Windows.Forms.Padding(2)
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemPictureEdit1})
+            Me.gridControl1.Size = New System.Drawing.Size(483, 468)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.DetailHeight = 284
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsView.ColumnAutoWidth = False
+            ' 
+            ' repositoryItemPictureEdit1
+            ' 
+            Me.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(483, 468)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Margin = New System.Windows.Forms.Padding(2)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private repositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private repositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+    End Class
 End Namespace
 
--- VB/WindowsApplication3/Form1.vb
+++ VB/WindowsApplication3/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -13,43 +12,44 @@ Imports DevExpress.Utils.Drawing
 
 
 Namespace WindowsApplication3
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-			InitData()
-			gridView1.Columns(0).Group()
-		End Sub
-		Public Sub InitData()
-			Dim dt = New DataTable()
-			For i As Integer = 0 To 14
-				dt.Columns.Add("col" & i)
-			Next i
-			For i As Integer = 0 To 9
-				dt.Rows.Add(New Object() { i Mod 2 })
-			Next i
-			gridControl1.DataSource = dt
-		End Sub
-		Private Sub gridView1_CustomDrawGroupRow(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventArgs) Handles gridView1.CustomDrawGroupRow
-			Dim view As GridView = TryCast(sender, GridView)
-			Dim viewInfo As GridViewInfo = TryCast(view.GetViewInfo(), GridViewInfo)
-			If view.LeftCoord = 0 Then
-				Return
-			End If
-			e.Appearance.FillRectangle(e.Cache, e.Bounds)
-			DrawGroupRow(e, viewInfo)
-			e.Handled = True
-		End Sub
-		Private Sub DrawGroupRow(ByVal e As DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventArgs, ByVal viewInfo As GridViewInfo)
-			Dim rowInfo As GridGroupRowInfo = TryCast(e.Info, GridGroupRowInfo)
-			Dim painter As GridGroupRowPainter = TryCast(e.Painter, GridGroupRowPainter)
-			rowInfo.ButtonBounds = GetButtonRectangle(rowInfo.ButtonBounds, viewInfo)
-			painter.ElementsPainter.GroupRow.DrawObject(rowInfo)
-		End Sub
-		Private Function GetButtonRectangle(ByVal origRect As Rectangle, ByVal viewInfo As GridViewInfo) As Rectangle
-			Dim buttonRect As Rectangle = origRect
-			buttonRect.X = viewInfo.ClientBounds.X + viewInfo.ViewRects.IndicatorWidth + 5
-			Return buttonRect
-		End Function
-	End Class
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            InitData()
+            gridView1.Columns(0).Group()
+        End Sub
+        Public Sub InitData()
+            Dim dt = New DataTable()
+            For i As Integer = 0 To 14
+                dt.Columns.Add("col" & i)
+            Next i
+            For i As Integer = 0 To 9
+                dt.Rows.Add(New Object() { i Mod 2 })
+            Next i
+            gridControl1.DataSource = dt
+        End Sub
+        Private Sub gridView1_CustomDrawGroupRow(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventArgs) Handles gridView1.CustomDrawGroupRow
+            Dim view As GridView = TryCast(sender, GridView)
+            Dim viewInfo As GridViewInfo = TryCast(view.GetViewInfo(), GridViewInfo)
+            If view.LeftCoord = 0 Then
+                Return
+            End If
+            e.Appearance.FillRectangle(e.Cache, e.Bounds)
+            DrawGroupRow(e, viewInfo)
+            e.Handled = True
+        End Sub
+        Private Sub DrawGroupRow(ByVal e As DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventArgs, ByVal viewInfo As GridViewInfo)
+            Dim rowInfo As GridGroupRowInfo = TryCast(e.Info, GridGroupRowInfo)
+            Dim painter As GridGroupRowPainter = TryCast(e.Painter, GridGroupRowPainter)
+            rowInfo.ButtonBounds = GetButtonRectangle(rowInfo.ButtonBounds, viewInfo)
+            painter.ElementsPainter.GroupRow.DrawObject(rowInfo)
+        End Sub
+        Private Function GetButtonRectangle(ByVal origRect As Rectangle, ByVal viewInfo As GridViewInfo) As Rectangle
+            Dim buttonRect As Rectangle = origRect
+            buttonRect.X = viewInfo.ClientBounds.X + viewInfo.ViewRects.IndicatorWidth + 5
+            Return buttonRect
+        End Function
+    End Class
 End Namespace
